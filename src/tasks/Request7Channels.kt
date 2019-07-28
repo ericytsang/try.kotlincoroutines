dbda3b9f71b4bebaf3f9a97d1023558063cea6a7
Eric Tsang <surplus.et@gmail.com>
2019-07-28 02:34:47 -0700

    finished task 7: using coroutines and channels to load organization contributor data concurrently while publishing progress updates to the UI

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -11,6 +11,27 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .bodyList()
+
+        val channel = Channel<List<User>>(Channel.UNLIMITED)
+
+        repos.forEach { repo ->
+            launch {
+                val users = service
+                    .getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+                channel.send(users)
+            }
+        }
+
+        val allUsers = mutableListOf<User>()
+        repeat(repos.size) {
+            allUsers += channel.receive()
+            updateResults(allUsers.aggregate(),it == repos.lastIndex)
+        }
     }
 }
