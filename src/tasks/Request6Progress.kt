f8154fb97b1b27ecf2d922ff42fcb1af00fd2bba
Eric Tsang <surplus.et@gmail.com>
2019-07-28 01:22:40 -0700

    finished task 6: show progress while using coroutines to load organization contributors synchronously

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -1,11 +1,27 @@
 package tasks
 
 import contributors.*
+import java.util.concurrent.atomic.AtomicInteger
 
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val contributors = mutableListOf<User>()
+
+    repos.forEachIndexed { i,repo ->
+        contributors += service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+        updateResults(
+            contributors.aggregate(),
+            i == repos.lastIndex)
+    }
 }
